f00f1b028051811b654fd3b5ab4443af65ae074b
Fernando Alcocer <fernandoalcocer812@hotmail.com>
2023-12-09 21:46:25 -0300

    probando con cambiar de https a http

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://feralcocer.github.io/Citas-Vet-React",
+  base: "http://feralcocer.github.io/Citas-Vet-React",
 });
